694fc62609affdf58a6c7f2a8e3d88db429ce3f6
Thatcher Auten <thatcherauten24@gmail.com>
2023-01-18 11:16:15 -0600

    range

--- ThreeArrayRange.java
+++ ThreeArrayRange.java
@@ -1,5 +1,23 @@
 public class ThreeArrayRange {
     public static void main(String[] args) {
-        int[] a = {2, 1, 3};
+        
+        int[] a = {67, 13, 45};
+        int largest = a[0];
+        int smallest = a[0];
+
+        if(largest < a[1]) {
+            largest = a[1];
+        }
+        if(smallest > a[1]) {
+            smallest = a[1];
+        }
+        if(largest < a[2]) {
+            largest = a[2];
+        }
+        if(smallest > a[2]) {
+            smallest = a[2];
+        }
+        System.out.println(largest - smallest);
+
     }
 }
